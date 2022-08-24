cca188f8f27ee0a706570f9e11e62fefb0a6fac5
IrinkaCh <123irina00@mail.ru>
2022-08-24 19:07:34 +0500

    Task 01 Find Positive Number

---
+++ Task01FindPositiveNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите количество чисел ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[M];
+for (int i = 0; i < M; i++)
+{
+    Console.WriteLine($"Введите {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int PrintArray (int [] array)
+{
+int count = 0;
+for(int i = 0; i < M; i++) 
+        { 
+            if(array[i] > 0)
+            {count = count +1;}
+        }
+ return count;
+}
+Console.WriteLine(PrintArray(array));
